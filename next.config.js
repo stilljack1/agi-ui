eee4b6e147901fe27ad4793addf35702a6ba3b0b
Jack AGI <jack@agi.com>
2025-12-20 13:26:26 +0000

    Fix: Switch to Static Export to Resolve Output Directory Error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
   },
 }
 module.exports = nextConfig
